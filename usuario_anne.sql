720fe01101882ad2c2bb203c6dde123e63d775b3
Paulo Cassel <110434600+PauloCassel@users.noreply.github.com>
2022-11-25 21:11:09 -0300

    Create usuario_anne.sql

---
+++ usuario_anne.sql
@@ -0,0 +1,12 @@
+use lol;
+
+SELECT * FROM vencedor;
+SELECT * FROM times;
+SELECT * FROM patrocinadores;
+SELECT * FROM personagens;
+SELECT * FROM jogadores;
+SELECT * FROM itens;
+SELECT * FROM partidas;
+
+INSERT INTO personagens VALUES
+('318', 'paulo');
