d2467f01a158abe8c267a14522306da01ed26a00
Paulo Cassel <110434600+PauloCassel@users.noreply.github.com>
2022-11-25 21:10:18 -0300

    Create usuario_paulo.sql

---
+++ usuario_paulo.sql
@@ -0,0 +1,15 @@
+use lol;
+
+SELECT * FROM maior_patrocinio;
+
+SELECT * FROM item_mais_caro;
+
+SELECT * FROM maior_premio;
+
+SELECT * FROM media_preco_itens;
+
+SELECT * FROM menor_premio;
+
+/* Teste */
+
+SELECT * FROM patrocinadores
